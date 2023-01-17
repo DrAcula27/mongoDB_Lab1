91c6840d00e6b5dc4b8fce5e67b8f2a42f35fae4
Danielle <danielle@kendallspace.net>
2023-01-17 17:11:02 -0500

    fix error in veggies.js trying to change myfruits, update homepage

--- models/veggies.js
+++ models/veggies.js
@@ -11,7 +11,7 @@ const veggieSchema = new mongoose.Schema({
 
 // We make a model from our Schema and point it at the collection we want
 // we can the use the model to create, read, update, and delete data in that collection
-const MyVeggie = mongoose.model("myfruits", veggieSchema);
+const MyVeggie = mongoose.model("myveggies", veggieSchema);
 // https://mongoosejs.com/docs/models.html
 // The first argument is the singular name of the collection your model is for. Mongoose automatically looks for the plural, lowercased version of your model name. Thus, for the example above, the model MyVeggie is for the myveggies collection in the database.
 
